d43d06f385d0a5b6efc112cbf47b4ec554e21b47
Renata Sendreti Broder <r.sendreti.broder@avanade.com>
2022-05-26 11:48:46 -0300

    include supplier data for in memory database

--- src/app/app-data.ts
+++ src/app/app-data.ts
@@ -3,13 +3,15 @@ import { ProductCategory } from './product-category/product-category';
 import { ProductCategoryData } from './product-category/product-category-data';
 import { Product } from './product/product';
 import { ProductData } from './product/product-data';
+import { Supplier } from './supplier/supplier';
+import { SupplierData } from './supplier/supplier-data';
 
 export class AppData implements InMemoryDbService {
 
-    createDb(): { products: Product[], productCategories: ProductCategory[] } {
+    createDb(): { products: Product[], productCategories: ProductCategory[], suppliers: Supplier[] } {
         const products = ProductData.products;
         const productCategories = ProductCategoryData.categories;
-        //const suppliers = SupplierData.suppliers;
-        return { products, productCategories };
+        const suppliers = SupplierData.suppliers;
+        return { products, productCategories, suppliers };
     }
 }
